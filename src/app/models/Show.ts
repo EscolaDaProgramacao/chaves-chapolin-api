3f5c4bca0d2f288e7dbd8eb4c078092c3220fe48
Fábio Diniz <fabioadrjf@gmail.com>
2023-11-05 20:18:00 -0300

    adicionado modelo e base de dados inicial

---
+++ src/app/models/Show.ts
@@ -0,0 +1,46 @@
+import db from "@/lib/db";
+
+import { CastMember } from "./CastMember";
+import { Character } from "./Character";
+import { Episode } from "./Episode";
+import { Quote } from "./Quote";
+
+export class Show {
+    id: number;
+    name: string;
+    description: string;
+
+    constructor(id: string) {
+        const show = db.data.shows.find(e => e.id === Number(id));
+
+        if (!show) {
+            throw new Error("Show not found!");
+        }
+
+        this.id = show.id;
+        this.name = show.name;
+        this.description = show.description;
+    }
+
+    getCastMembers(): CastMember[] {
+        const castMemberIds = db.data.roles
+            .filter(s => s.showId === this.id)
+            .map(role => role.castMemberId);
+
+        const castMembers = db.data.castMembers.filter(s => castMemberIds.includes(s.id));
+
+        return castMembers;
+    }
+
+    getCharacters(): Character[] {
+        return db.data.characters.filter(e => e.showId === this.id);
+    }
+
+    getEpisodes(): Episode[] {
+        return db.data.episodes.filter(e => e.showId === this.id);
+    }
+
+    getQuotes(): Quote[] {
+        return db.data.quotes.filter(e => e.showId === this.id);
+    }
+}
